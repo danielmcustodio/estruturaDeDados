2dc556927408e6410e7027edc2d04ff1b2d06a6a
Daniel Custodio <daniel.custodio@univille.br>
2017-03-20 22:05:50 -0300

    Testando ibovespa

--- aula_05/test/com/bovespa/StockManagerTest.java
+++ aula_05/test/com/bovespa/StockManagerTest.java
@@ -7,9 +7,15 @@ import org.junit.Test;
 public class StockManagerTest {
 	
 	@Test
-	public void loadStocks() throws IOException{
+	public void loadStocksTest() throws IOException{
 		StockManager stockManager = new StockManager();
-		stockManager.loadStocks("Z:/Ibovespa/COTAHIST_A2016_TEST.txt");
+		Stock[] stocks = stockManager.loadStocks("Z:/Ibovespa/COTAHIST_A2016.txt");
+		
+		for (Stock stock : stocks) {
+			if(stock.getCodigo().equals("PETR4") && stock.getData().getDayOfMonth() == 21 & stock.getData().getMonthValue() == 3 & stock.getData().getYear() == 2016){
+				System.out.println(stock.getCodigo() + "\t" + stock.getData() + "\t" + stock.getOpenPrice() + "\t" + stock.getMinPrice() + "\t" + stock.getMaxPrice());
+			}
+		}
 	}
 
 }
