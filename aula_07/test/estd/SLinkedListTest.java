e4e85d6e31d7cc16226a0dbf4f90218126993e15
Daniel Custodio <daniel.custodio@univille.br>
2017-04-10 21:59:41 -0300

    testes

---
+++ aula_07/test/estd/SLinkedListTest.java
@@ -0,0 +1,49 @@
+package estd;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SLinkedListTest {
+	
+	@Test
+	public void getSizeTest(){
+		SLinkedList<String> linkedList = new SLinkedList<>();
+		Assert.assertTrue(linkedList.getSize() == 0);
+		Assert.assertTrue(linkedList.isEmpty());
+		
+		Node<String> nodeA = new Node<>("A");
+		Node<String> nodeB = new Node<>("B");
+		Node<String> nodeC = new Node<>("C");
+		
+		
+		linkedList.add(nodeA);
+		linkedList.add(nodeB);
+		linkedList.add(nodeC);
+		
+		Assert.assertTrue(linkedList.getSize() == 3);
+		Assert.assertFalse(linkedList.isEmpty());
+		
+	}
+	
+	@Test
+	public void removeFirstTest(){
+		SLinkedList<String> linkedList = new SLinkedList<>();
+		
+		Node<String> nodeA = new Node<>("A");
+		Node<String> nodeB = new Node<>("B");
+		Node<String> nodeC = new Node<>("C");
+		
+		
+		linkedList.add(nodeA);
+		linkedList.add(nodeB);
+		linkedList.add(nodeC);
+		
+		linkedList.removeFirst();
+		
+		Assert.assertTrue(linkedList.getSize() == 2);
+		Assert.assertFalse(linkedList.isEmpty());
+		
+	}
+	
+	
+}
